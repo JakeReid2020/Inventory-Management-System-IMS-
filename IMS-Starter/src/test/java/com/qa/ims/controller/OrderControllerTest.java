ea56208a0facf508dc27f3aff69e62b883752d55
jake.reid <jake.reid@qa.com>
2021-01-07 12:34:05 +0000

    final error removed on Order Controller Test

--- IMS-Starter/src/test/java/com/qa/ims/controller/OrderControllerTest.java
+++ IMS-Starter/src/test/java/com/qa/ims/controller/OrderControllerTest.java
@@ -11,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.anyLong;
 
 import com.qa.ims.controller.OrderContoller;
 import com.qa.ims.persistence.dao.CustomerDAO;
